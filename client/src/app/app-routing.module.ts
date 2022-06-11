17277112e0bc2e5cf254be7cffdb33b66579bbe6
ItaloKornejo <italokornejo@gmail.com>
2022-06-11 16:32:44 -0500

    4-10 Jun

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -53,3 +53,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,9 +1,12 @@
 export default {
 
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '4321',
-        database: 'mydb'
+        host: 'bi2pq1g708m0.us-east-4.psdb.cloud',
+        user: 'yl4j74k4rf97',
+        password: 'pscale_pw_ROCdoa3POr5Tu6uxQg00XoN9Lhuc0N-DPaMXp_kNHyc',
+        database: 'lmz-skool',
+        ssl: {
+            rejectUnauthorized: true
+        }
     }
 }
