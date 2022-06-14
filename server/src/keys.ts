2348959de14536bdb562d6b26ab3c099b3c94a18
ItaloKornejo <italokornejo@gmail.com>
2022-06-14 10:13:24 -0500

    update database

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -2,8 +2,8 @@ export default {
 
     database:{
         host: 'bi2pq1g708m0.us-east-4.psdb.cloud',
-        user: 'iwcvjdkqcr22',
-        password: 'pscale_pw_KUmLwFRGDlZOBADABabDqMAbAcxZOiSXg1LVG8-Z9PM',
+        user: 'fqf1ksozaeq7',
+        password: 'pscale_pw_Hm1pyMKp0RXf6FGuoFyCV7EyYH9UiQXWwcSDK7N8uPc',
         database: 'lmz-skool',
         ssl: {
             rejectUnauthorized: true
